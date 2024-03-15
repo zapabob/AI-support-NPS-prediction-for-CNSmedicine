7d7568fba87ef4b5bd05108626bfddacdfd2d203
峯岸　亮 <1920071390@campus.ouj.ac.jp>
2024-03-16 08:22:18 +0900

    Update descriptor_utils.py

--- docs/utils/descriptor_utils.py
+++ docs/utils/descriptor_utils.py
@@ -1,43 +1,52 @@
-from rdkit.Chem import Descriptors
+import pandas as pd
+from rdkit import Chem
+from chembl_webresource_client.new_client import new_client
+from sklearn.preprocessing import StandardScaler
+from dgllife.utils import smiles_to_bigraph, CanonicalAtomFeaturizer, CanonicalBondFeaturizer
 
-def calculate_descriptors(mol):
-    """Calculate descriptors for a given compound"""
-    descriptors = []
+# Internal data: SMILES of standard compounds
+standard_compounds = {
+    'D-Amphetamine': 'CC(N)Cc1ccccc1',
+    'Cocaine': 'COC(=O)C1C(OC(=O)c2ccccc2)CC2CCC1N2C',
+    'Methylphenidate': 'COC(=O)C(C1CCCCN1)c1ccccc1',
+    'LSD-25': 'CCN(CC)C(=O)C1CN(C)C2CC3=CNC4=CC=CC(=C34)C2=C1',
+    'MDMA': 'CC(NC)Cc1ccc2c(c1)OCO2',
+    '2C-B': 'CC(Cc1ccc2c(c1)OCO2)NC',
+    'Psilocybin': 'CN(C)CCc1c[nH]c2cccc(OP(=O)(O)O)c12',
+    'Mescaline': 'COc1cc(CC(=O)O)cc2c1occc2'
+}
 
-    # Basic descriptors
-    descriptors.append(Descriptors.MolWt(mol))  # Molecular weight
-    descriptors.append(Descriptors.HeavyAtomCount(mol))  # Number of heavy atoms
-    descriptors.append(Descriptors.NHOHCount(mol))  # Number of hydroxyl groups
-    descriptors.append(Descriptors.NOCount(mol))  # Number of nitrogen-oxygen groups
-    descriptors.append(Descriptors.NumHAcceptors(mol))  # Number of hydrogen bond acceptors
-    descriptors.append(Descriptors.NumHDonors(mol))  # Number of hydrogen bond donors
-    descriptors.append(Descriptors.NumHeteroatoms(mol))  # Number of heteroatoms
-    descriptors.append(Descriptors.NumRotatableBonds(mol))  # Number of rotatable bonds
-    descriptors.append(Descriptors.NumValenceElectrons(mol))  # Number of valence electrons
-    descriptors.append(Descriptors.TPSA(mol))  # Topological polar surface area
+def load_chembl_data(target_ids):
+    """Load data from ChEMBL for specified target IDs"""
+    activities = new_client.activity
+    res = activities.filter(target_chembl_id__in=target_ids).filter(standard_type="IC50")
+    df = pd.DataFrame.from_records(res)
+    df['pIC50'] = -np.log10(df['standard_value'])
+    return df
 
-    # Lipinski descriptors
-    descriptors.append(Descriptors.MolLogP(mol))  # LogP value
-    descriptors.append(Descriptors.MolMR(mol))  # Molar refractivity
+def normalize_data(data):
+    """Normalize the data"""
+    scaler = StandardScaler()
+    data['normalized_pIC50'] = scaler.fit_transform(data[['pIC50']])
+    return data, scaler
 
-    # Charge descriptors
-    descriptors.append(Descriptors.MaxPartialCharge(mol))  # Maximum partial charge
-    descriptors.append(Descriptors.MinPartialCharge(mol))  # Minimum partial charge
+def prepare_data(normalized_data):
+    """Prepare train, validation, and test data"""
+    X = normalized_data['canonical_smiles'].apply(lambda smiles: smiles_to_graph(smiles))
+    y = normalized_data['normalized_pIC50']
+    return X, y
 
-    # Geometric descriptors
-    descriptors.append(Descriptors.PMI1(mol))  # First principal moment of inertia
-    descriptors.append(Descriptors.PMI2(mol))  # Second principal moment of inertia
-    descriptors.append(Descriptors.PMI3(mol))  # Third principal moment of inertia
-    descriptors.append(Descriptors.RadiusOfGyration(mol))  # Radius of gyration
+def smiles_to_graph(smiles):
+    graph = smiles_to_bigraph(smiles, node_featurizer=CanonicalAtomFeaturizer(), edge_featurizer=CanonicalBondFeaturizer())
+    return graph
 
-    # Topological descriptors
-    descriptors.append(Descriptors.BalabanJ(mol))  # Balaban J value
-    descriptors.append(Descriptors.BertzCT(mol))  # Bertz CT value
-    descriptors.append(Descriptors.Chi0(mol))  # Connectivity index chi-0
-    descriptors.append(Descriptors.Chi1(mol))  # Connectivity index chi-1
-    descriptors.append(Descriptors.HallKierAlpha(mol))  # Hall-Kier alpha value
-    descriptors.append(Descriptors.Kappa1(mol))  # Kappa shape index 1
-    descriptors.append(Descriptors.Kappa2(mol))  # Kappa shape index 2
-    descriptors.append(Descriptors.Kappa3(mol))  # Kappa shape index 3
+def split_data(X, y, test_size=0.2, random_state=42):
+    """Split data into train, validation, and test sets"""
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=test_size, random_state=random_state)
 
-    return descriptors
+    train_data = list(zip(X_train, y_train))
+    val_data = list(zip(X_val, y_val))
+    test_data = list(zip(X_test, y_test))
+
+    return train_data, val_data, test_data
